f8eb2a1483532af61410ef64d545df065238928b
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-27 12:12:58 +0600

    banner section done

---
+++ my-project/src/components/Banner.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import bannerImage from '../assets/banner-jpg.jpg'
+
+const Banner = () => {
+    return (
+        <div className="hero bg-base-200 h-[400px]">
+
+            <div className="flex justify-between items-center gap-3">
+                <div >
+                <img
+                    src={bannerImage}
+                    
+                    className="max-w-sm rounded-lg shadow-2xl w-full h-full" />
+                </div>
+                <div >
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Book Appointment</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- my-project/src/components/Home.jsx
+++ my-project/src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from './Banner';
 
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='mx-10'>
+            <Banner />
             
         </div>
     );
