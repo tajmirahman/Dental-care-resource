9a96a05bc014f5d4f51a61986b8ee299119e70bc
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-27 11:15:35 +0600

    firebase initial setup done

---
+++ my-project/src/components/AllAppointment/AllAppointment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllAppointment = () => {
+    return (
+        <div>
+            <h1>Appointment</h1>
+        </div>
+    );
+};
+
+export default AllAppointment;
---
+++ my-project/src/components/AllTreatment/AllTreatment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllTreatment = () => {
+    return (
+        <div>
+            <h1>Treatment page</h1>
+        </div>
+    );
+};
+
+export default AllTreatment;
