da4a9671cde9cdc1b04255af785bbdd9433777fe
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-28 12:18:48 +0600

    login form design done

--- my-project/src/components/Login.jsx
+++ my-project/src/components/Login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            
+        <div className='bg-sky-600 w-[400px] h-[300px] md:w-[600px]  mx-auto  mt-5 p-6'>
+
+            <h1 className='text-center text-2xl text-white '>Signup Your Account</h1>
+
+            <form className='space-y-4 text-center p-2 mt-6'>
+                
+                <div>
+                    <input type="email" name="email" className='w-[70%]  text-center' placeholder="Your Email" required />
+                </div>
+                <div>
+                    <input type="password" name="password" className='w-[70%] text-center' placeholder="Your Password" required />
+                </div>
+               
+                
+
+
+                <button type="submit" className='btn'>Sign Up</button>
+                <p className='text-white'>If you have already an account? please <NavLink to={'/login'}><span className='text-yellow-500 underline'>login</span></NavLink></p>
+
+            </form>
+
+
+
         </div>
     );
 };
--- my-project/src/components/Register.jsx
+++ my-project/src/components/Register.jsx
@@ -1,34 +1,50 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div className="w-[70%] mx-auto mt-6 border-2 border-red-200 p-6">
-  <form className="flex flex-col items-center space-y-4">
-    <div className="w-full flex flex-col items-center gap-1">
-      <label className="text-center">Name</label>
-      <input type="text" name="text" className="input input-bordered w-full" required />
-    </div>
-    <div className="w-full flex flex-col items-center gap-1">
-      <label className="text-center">Photo Url</label>
-      <input type="text" name="photo" className="input input-bordered w-full" required />
-    </div>
-    <div className="w-full flex flex-col items-center gap-1">
-      <label className="text-center">Email</label>
-      <input type="email" name="email" className="input input-bordered w-full" required />
-    </div>
-    <div className="w-full flex flex-col items-center gap-1">
-      <label className="text-center">Password</label>
-      <input type="password" name="password" className="input input-bordered w-full" required />
-    </div>
-    <div className="w-full flex flex-col items-center gap-1">
-      <label className="text-center">Confirm Password</label>
-      <input type="password" name="conPassword" className="input input-bordered w-full" required />
-    </div>
-    <div>
-      <button className="btn btn-primary mt-2">Submit</button>
-    </div>
-  </form>
-</div>
+        <div className='bg-sky-600 w-[400px] md:w-[600px]  mx-auto  mt-5 '>
+
+            <h1 className='text-center text-2xl text-white mt-2'>Signup Your Account</h1>
+
+            <form className='space-y-2 text-center p-2'>
+                <div className='mt-2 '>
+                    <input type="text" name="name" className='w-[70%] text-center' placeholder="Your Name" />
+                </div>
+                <div className='mt-2 '>
+                    <input type="text" name="photoUrl" className='w-[70%] text-center' placeholder="Photo Url" />
+                </div>
+                <div>
+                    <input type="email" name="email" className='w-[70%]  text-center' placeholder="Your Email" required />
+                </div>
+                <div>
+                    <input type="password" name="password" className='w-[70%] text-center' placeholder="Your Password" required />
+                </div>
+                <div>
+                    <input type="password" name="conPassword" className='w-[70%] text-center' placeholder="Your Password" required />
+                </div>
+                <div className=" text-red-700 w-[60%] mx-auto  rounded-lg p-2 ">
+                    
+                </div>
+
+
+                <button type="submit" className='btn'>Sign Up</button>
+                <p className='text-white'>If you have already an account? please <NavLink to={'/login'}><span className='text-yellow-500 underline'>login</span></NavLink></p>
+
+            </form>
+
+            <hr />
+            <div className='text-center space-y-2 p-2'>
+                <h1 className='text-white'>Or Sign up with</h1>
+                <div>
+                    <button className='btn'>with google</button>
+                </div>
+                {/* <div>
+                    <button className='btn btn-warning'>with github</button>
+                </div> */}
+            </div>
+
+        </div>
 
     );
 };
