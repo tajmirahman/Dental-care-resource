2322783eeb3083f6a8d9a594e21e1fd3b34a31b4
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-28 11:31:10 +0600

    now modal form work is proccecing

--- my-project/src/components/Modal.jsx
+++ my-project/src/components/Modal.jsx
@@ -6,16 +6,30 @@ const Modal = () => {
 
             <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box bg-gray-200">
-                    <form action="">
-                        <div className='space-x-4'>
-                            <label >Full Name</label>
-                            <input type="text" name='text' required />
+                    <form className="space-y-3">
+                        <div className="flex items-center gap-4">
+                            <label className="w-32">Full Name</label>
+                            <input type="text" name="text" required className="input input-bordered flex-1" />
                         </div>
-                        <div className='space-x-4'>
-                            <label >Full Name</label>
-                            <input type="text" name='text' required />
+                        <div className="flex items-center gap-4">
+                            <label className="w-32">Email</label>
+                            <input type="email" name="email" required className="input input-bordered flex-1" />
                         </div>
+                        <div className="flex items-center gap-4">
+                            <label className="w-32">Phone</label>
+                            <input type="text" name="number" required className="input input-bordered flex-1" />
+                        </div>
+                        <div className="flex items-center gap-4">
+                            <label className="w-32">Appointment Date</label>
+                            <input type="date" name="date" required className="input input-bordered flex-1" />
+                        </div>
+                        <div className="flex items-center gap-4">
+                            <label className="w-32">Address</label>
+                            <input type="text" name="address" required className="input input-bordered flex-1" />
+                        </div>
+                        <button className="btn btn-primary">Make Appointment</button>
                     </form>
+
                     <div className="modal-action">
                         <form method="dialog">
                             {/* if there is a button in form, it will close the modal */}
